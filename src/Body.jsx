da5d65051696dfdc98fbfba2d592e761b337c2f2
Athithya <athithyakarthikeyan@gmail.com>
2024-06-10 21:13:37 +0530

    Added Framer motion framework

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Card from './Card.jsx';
 import Image from './assets/Wordle_Logo.svg.png';
 import Password from './assets/password-icon-protection-illustration-sign-security-symbol-or-logo-vector-removebg-preview.png';
@@ -14,14 +14,21 @@ import Linkedin from "./assets/png-clipart-linkedin-linkedin-thumbnail-removebg-
 import Github from "./assets/GitHub-Mark-ea2971cee799-removebg-preview.png";
 import Weather from "./assets/images.png";
 import Skills from './Skills.jsx';
+import { motion, useInView } from 'framer-motion';
 
 function Body() {
+  const skillsRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const skillsInView = useInView(skillsRef, { once: true });
+  const contactInView = useInView(contactRef, { once: true });
+
   return (
     <div className="main-body">
       <div className="about-me">
         <div className="about-content">
           <img className="pfp" src={Pfp} alt="Profile Picture" />
-          <p className="intro-text"><strong>Hey there! I'm Athithya, a second year computer science student with a passion of creating stunning websites!</strong></p>
+          <p className="intro-text"><strong>Hey there! I'm Athithya, a second-year computer science student with a passion for creating stunning websites!</strong></p>
         </div>
       </div>
       <div id="project" className="project-section">
@@ -30,49 +37,65 @@ function Body() {
       <div className="project-cards">
         <Card image={Image} style={{ width: '100px', marginTop: '50px', marginBottom: '50px' }} lang="PYTHON" name="Wordle" description="Wordle Game consisting of 300 words and simple GUI" link="https://github.com/athithyakarthikeyan/passwordGenerator" />
         <Card image={Password} style={{ width: '200px' }} lang="PYTHON" name="Password Generator" description="Password Generator with variable length and saving options" link="https://github.com/athithyakarthikeyan/passwordGenerator" />
-        <Card image={Weather} style={{ width: '200px' }} lang="JAVASCRIPT" name="Weather App" description="Weather app that uses React.js and OpenWeatherAPP api" link="https://github.com/athithyakarthikeyan/weatherApp" />
+        <Card image={Weather} style={{ width: '200px' }} lang="JAVASCRIPT" name="Weather App" description="Weather app that uses React.js and OpenWeatherAPI" link="https://github.com/athithyakarthikeyan/weatherApp" />
         <Card image={Bank} style={{ width: '200px' }} lang="C++" name="Bank Account" description="Bank account simulator using OOPs consisting of useful functions" link="https://github.com/athithyakarthikeyan/bankSystem" />
       </div>
       <div className="certificate-section">
-        <h1 className="certificate-title">Skills</h1>
+        <motion.div
+          className="profile-button"
+          initial={{ opacity: 0, x: -100 }}
+          ref={skillsRef}
+          animate={skillsInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1 }}
+        >
+          <h1 className="certificate-title">Skills</h1>
+        </motion.div>
         <div className="certificates">
-          <Skills skillName={"HTML"}skillLogo ={HTML} style={{width:"150px"}} skillPercent={"90"}/>
-          <Skills skillName={"CSS"}skillLogo ={CSS} style={{width:"130px"}}skillPercent={"80"}/>
-          <Skills skillName={"JavaScript"}skillLogo ={Javascript} style={{width:"130px"}}skillPercent={"60"}/>
-          <Skills skillName={"React.js"}skillLogo ={Reactjs} style={{width:"130px"}}skillPercent={"80"}/>
+          <Skills skillName={"HTML"} skillLogo={HTML} style={{ width: "150px" }} skillPercent={"90"} />
+          <Skills skillName={"CSS"} skillLogo={CSS} style={{ width: "130px" }} skillPercent={"80"} />
+          <Skills skillName={"JavaScript"} skillLogo={Javascript} style={{ width: "130px" }} skillPercent={"60"} />
+          <Skills skillName={"React.js"} skillLogo={Reactjs} style={{ width: "130px" }} skillPercent={"80"} />
         </div>
       </div>
       <div id="contact" className="contact-section">
-        <h1 className="contact-title">Contact</h1>
+        <motion.div
+          className="contact-title"
+          initial={{ opacity: 0, x: -100 }}
+          ref={contactRef}
+          animate={contactInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1 }}
+        >
+          <h1>Contact</h1>
+        </motion.div>
         <div className="contact-content">
-            <div className="resume">
-             <h1>Resume</h1>
-             <p>A copy of my resume with all my qualifications and details</p>
-             <button onClick={downloadFile} className="download-button">Download</button>
-             <h1>Socials</h1>
-             <div className="socials">
+          <div className="resume">
+            <h1>Resume</h1>
+            <p>A copy of my resume with all my qualifications and details</p>
+            <button onClick={downloadFile} className="download-button">Download</button>
+            <h1>Socials</h1>
+            <div className="socials">
               <a href="https://www.linkedin.com/in/athithya-karthikeyan-558226258/"><img className="social-image" src={Linkedin} alt="LinkedIn" /></a>
               <a href='https://github.com/athithyakarthikeyan'><img className="social-image" src={Github} alt="GitHub" /></a>
-             </div>
             </div>
-            <div className="mail-section">
-              <h1>Mail me!</h1>
+          </div>
+          <div className="mail-section">
+            <h1>Mail me!</h1>
             <form className="form" action="mailto:athithyakarthikeyan@gmail.com" method="post" encType="text/plain">
-                <div className="form-group">
-                    <label htmlFor="name">Name: </label>
-                    <input type="text" id="name" name="name" required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message: </label>
-                    <textarea id="message" name="message" required></textarea>
-                </div>
-                <button className="send-button" type="submit">Send</button>
-              </form>
-            </div>
+              <div className="form-group">
+                <label htmlFor="name">Name: </label>
+                <input type="text" id="name" name="name" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email: </label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message: </label>
+                <textarea id="message" name="message" required></textarea>
+              </div>
+              <button className="send-button" type="submit">Send</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,22 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./index.css";
+import { motion } from "framer-motion";
 
-export default function Header({ aboutRef, projectRef, contactRef }) {
+export default function Header() {
   return (
     <div className="header">
       <nav className="nav-bar">
         <h1 className="main-title">Athithya Karthikeyan</h1>
-        <div className="profile-button">
+        <motion.div
+          className="profile-button"
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+        >
           <h1>Front-end Developer</h1>
-        </div>
+        </motion.div>
       </nav>
+
     </div>
   );
 }
